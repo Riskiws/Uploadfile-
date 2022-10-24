0b78d70a9fde4d72c419d6790e6edf286f4cc0e9
Riskiws <116546039+Riskiws@users.noreply.github.com>
2022-10-24 17:04:46 +0700

    Add files via upload

---
+++ For t while 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int y= 1;
+    while ( y <=1000){
+printf ("loopke%i\n",y);
+       y++;
+}
+
+
+for (int x=1 ; x <= 666;x++){
+    cout << x << endl;}
+    cout << "yahahayyukk" <<endl;
+}
---
+++ nested-loop2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int j;
+        for(j=10 ; j>0 ; j--)
+            printf("angka%i \n",j);
+}
+         
